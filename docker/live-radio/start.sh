1fa8700d6c6bf10d12f735424cbd3f26ec90133b
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-03-12 13:07:16 +0530

    fix: docker issue

--- docker/live-radio/start.sh
+++ docker/live-radio/start.sh
@@ -9,8 +9,5 @@ node src/server.js &
 # Start FFmpeg streaming
 node src/stream-manager.js &
 
-# Wait for any process to exit
-wait -n
-
-# Exit with status of process that exited first
-exit $?
+# Keep container alive
+wait
