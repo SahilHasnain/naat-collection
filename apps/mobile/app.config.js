1deb73a72d2e65e8168df24698a82a861ed76160
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-04-08 18:31:09 +0530

    again prebuild

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -40,7 +40,7 @@ export default {
       associatedDomains: ["applinks:owaisrazaqadri.appwrite.network"],
     },
     android: {
-      versionCode: 11,
+      versionCode: 12,
       adaptiveIcon: {
         foregroundImage: "./assets/images/android-icon-foreground.png",
         backgroundColor: "#000000",
