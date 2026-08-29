d0c2837b2193d1d028d8749d111409b697126dbb
SAHIL HASNAIN <86091126+SahilHasnain@users.noreply.github.com>
2026-08-29 20:22:24 +0530

    Increment versionCode from 26 to 27

--- apps/mobile/brand.config.js
+++ apps/mobile/brand.config.js
@@ -19,7 +19,7 @@ module.exports = {
     packageId: "com.owaisrazaqadri", // production package
     packageIdDev: "com.owaisrazaqadri.dev",
     packageIdPreview: "com.owaisrazaqadri.preview",
-    versionCode: 26,
+    versionCode: 27,
     // Deep-link host for universal links (used in intent filters + associatedDomains)
     applinksHost: "owaisrazaqadri.appwrite.network",
   },
