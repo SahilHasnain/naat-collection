6f2ac1c2db9100a0986ba60d03542cd54b9a60c3
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-08-30 23:39:07 +0530

    Bump versionCode to 28

--- apps/mobile/brand.config.js
+++ apps/mobile/brand.config.js
@@ -19,7 +19,7 @@ module.exports = {
     packageId: "com.owaisrazaqadri", // production package
     packageIdDev: "com.owaisrazaqadri.dev",
     packageIdPreview: "com.owaisrazaqadri.preview",
-    versionCode: 27,
+    versionCode: 28,
     // Deep-link host for universal links (used in intent filters + associatedDomains)
     applinksHost: "owaisrazaqadri.appwrite.network",
   },
